2a833a38097f35bc21ecc688201cf2553378d9b9
BettySinister <abc_def.3000@mail.ru>
2022-11-19 22:52:56 +0300

    first commit

---
+++ exam1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream> 
+using namespace std;
+
+int main() {
+	int input;
+	cout << "Enter the amount of lines to display: ";
+	cin >> input;
+	for (int i = 1; i < input + 1; i++) {
+		for (int j = 1; j < 11; j++) {
+			cout << i * j << ' ';
+			if (i * j < 10) cout << ' ';
+		}
+		cout << endl;
+	}
+}
